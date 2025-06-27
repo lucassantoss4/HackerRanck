b95458ca059cff6d21ab6850bec5418e2ddf9b8d
lucassantoss4 <lucass4@al.insper.edu.br>
2025-06-27 15:26:19 -0300

    modified:   encontreMediana.py

--- encontreMediana.py
+++ encontreMediana.py
@@ -1,7 +1,10 @@
-arr = [5,3,2,1,4]
+def findMedian(arr):
+    # Write your code here
 
-arr_convert = sorted(arr)
+    arr_convert = sorted(arr)
 
-tam_arr = (len(arr)//2)
-resultado = arr_convert[tam_arr]
-print(resultado)
+    tam_arr = (len(arr)//2)
+    resultado = arr_convert[tam_arr]
+    print(resultado)
+
+    return resultado
